7c4b16a823e9363d3507565d5a1bcc947d748813
kazboo <kaz.orita@gmail.com>
2020-02-04 00:26:05 +0900

    add: editor sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Editor from '@monaco-editor/react';
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// const App = () => <Editor height="90vh" language="javascript" />;
+const App = () => <Editor height="90vh" language="json" />;
 
 export default App;
